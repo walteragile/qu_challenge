676a92620c7d7b46cd0d8bdf673d6d2178545453
walteragile <wcarpiom@gmail.com>
2023-12-06 18:49:49 -0500

    US02 Develop matrix entity class

---
+++ src/QU.Core/Entities/Matrix.cs
@@ -0,0 +1,18 @@
+﻿namespace QU.Core.Entities;
+
+public class Matrix
+{
+    char[,] _data;
+
+    public Matrix(int xDimension, int yDimension)
+    {
+        if (xDimension > 64 || yDimension > 64)
+        {
+            throw new ArgumentException("Any dimension should not exceed 64");
+        }
+        _data = new char[xDimension, yDimension];        
+    }
+
+    public int XDimension { get => _data.GetLength(0); }
+    public int YDimension { get => _data.GetLength(1); }
+}
---
+++ test/QU.Core.Test/Entities/MatrixTests.cs
@@ -0,0 +1,17 @@
+﻿using QU.Core.Entities;
+
+namespace QU.Core.Test.Entities;
+
+public class MatrixTests
+{
+    Matrix _target;
+
+    [Fact]
+    public void OnInvalidDimensions_ThrowsAnException()
+    {
+        // Arrange
+        var dimension = 65;
+
+        Assert.Throws<ArgumentException>(() => _target = new Matrix(dimension, dimension));
+    }
+}
